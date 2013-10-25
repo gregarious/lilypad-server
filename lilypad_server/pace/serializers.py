07417387b66f181c7e41b263da6f7ed74c20c0af
Greg Nicholas <greg.nicholas@gmail.com>
2013-10-25 14:03:16 -0400

    fix deprecation warning

--- lilypad_server/pace/serializers.py
+++ lilypad_server/pace/serializers.py
@@ -111,7 +111,7 @@ class PointLossSerializer(NamespacedHyperlinkedModelSerializer):
 
 class PeriodicRecordSerializer(NamespacedHyperlinkedModelSerializer):
     student = serializers.PrimaryKeyRelatedField()
-    point_losses = PointLossSerializer(many=True, blank=True)
+    point_losses = PointLossSerializer(many=True, required=False)
 
     class Meta:
         model = PeriodicRecord
